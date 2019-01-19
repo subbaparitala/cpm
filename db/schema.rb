a7c0df36d11b6d8833d589f435136c5bd8791fdd
subbaparitala <subbaparitala@gmail.com>
2019-01-19 17:19:48 -0600

    create rake task to import csv files to databases

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_203636) do
+ActiveRecord::Schema.define(version: 2019_01_19_163410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
